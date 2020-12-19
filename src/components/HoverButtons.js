7277f5c0ab3c96c282cd587925b8ab56524dcc1c
Shantanu Acharya <thegeek.004@gmail.com>
2020-12-19 19:28:18 +0530

    HoverButtons component can now support input props of both array and object type

--- src/components/HoverButtons.js
+++ src/components/HoverButtons.js
@@ -2,18 +2,32 @@ import _ from 'lodash';
 import React from 'react';
 
 class HoverButtons extends React.Component {
-  renderButton({ buttonValue, buttonText }) {
+  renderButton(buttonData) {
+    let buttonValue = '';
+    let buttonText = '';
+
+    if (buttonData instanceof Object) {
+      buttonValue = buttonData.buttonValue;
+      buttonText = buttonData.buttonText;
+    } else {
+      buttonValue = buttonData;
+      buttonText = buttonData;
+    }
+
     return (
       <button
         className={`btn border border-secondary mx-2 btn-hover ${
           this.props.isSmall ? 'btn-sm' : ''
-        } ${this.props.currentButtonValue === buttonValue ? 'btn-info' : ''}`}
-        onClick={() => {
+        } ${
+          this.props.currentButtonValue === buttonValue ? 'btn-primary' : ''
+        }`}
+        onClick={event => {
+          event.preventDefault();
           this.props.changeCurrentButtonValue(buttonValue);
         }}
         key={buttonValue}
       >
-        {buttonText}
+        {buttonText === undefined ? buttonValue : buttonText}
       </button>
     );
   }
@@ -21,7 +35,9 @@ class HoverButtons extends React.Component {
   render() {
     return (
       <React.Fragment>
-        {_.map(this.props.hoverButtons, button => this.renderButton(button))}
+        {_.map(this.props.hoverButtons, buttonData =>
+          this.renderButton(buttonData)
+        )}
       </React.Fragment>
     );
   }
